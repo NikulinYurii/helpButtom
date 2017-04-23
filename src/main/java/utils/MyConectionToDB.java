33001ddd47f812344fd5f13301c14d8dccdc65b4
Your Name <you@example.com>
2017-04-23 17:15:39 +0300

    changed class server

--- src/main/java/utils/MyConectionToDB.java
+++ src/main/java/utils/MyConectionToDB.java
@@ -1,5 +1,6 @@
 package utils;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
